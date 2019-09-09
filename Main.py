ffc995844b44aff9a3825a6dc0b3b167720f4eb7
Stets42 <basti123s@googlemail.com>
2019-09-09 12:07:11 +0200

    PageLinkExtractor fully working

--- Main.py
+++ Main.py
@@ -3,13 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 from UrlProvider import UrlProvider
 
-link_extractor = PageLinkExtractor()
-#for link in link_extractor.extract_links("https://www.cyberport.de/notebook-und-tablet/notebooks.html"):
-#    print(link)
-
-#request = requests.get("https://www.cyberport.de/notebook-und-tablet/notebooks.html")
-#page = BeautifulSoup(request.text, "html.parser")
-
+request = requests.get("https://www.cyberport.de/notebook-und-tablet/notebooks.html")
+page = BeautifulSoup(request.text, "html.parser")
 
 def get_total_page_count():
     numbers = []
@@ -20,11 +15,6 @@ def get_total_page_count():
     return a[20]
 
 
-url_provider = UrlProvider
-all_links = url_provider.url_all_pages("descending", 1, 5)
-
-for url in all_links:
-    all_links.append(link_extractor.extract_links(url))
-    print(url)
-
-print(len(all_links))
+url_provider = UrlProvider(get_total_page_count())
+urls = url_provider.url_all_pages("descending", 1, 5)
+print(urls)
--- UrlProvider.py
+++ UrlProvider.py
@@ -2,6 +2,9 @@ from urllib.parse import urljoin
 
 
 class UrlProvider:
+    def __init__(self, max_page_count):
+        self.max_page_count = max_page_count
+
     def create_start_url(self, sort_type):
         if sort_type == "ascending":
             return "https://www.cyberport.de/notebook-und-tablet/notebooks.html?productsPerPage=24&sort=price_asc&page="
@@ -18,5 +21,5 @@ class UrlProvider:
     def url_all_pages(self, sort_type, start, stop):
         url_list = []
         for i in range(start, stop):
-            url_list.append(urljoin(self.create_start_url(sort_type), str(range(start, stop))))
+            url_list.append(self.create_start_url(sort_type) + str(i))
         return url_list
