be7351533baeb03fcbda44e0f667e42e478971e7
Stets42 <basti123s@googlemail.com>
2019-09-09 12:57:54 +0200

    added LaptopEntry added build_article_url function to UrlProvider

--- PageLinkExtractor.py
+++ PageLinkExtractor.py
@@ -6,14 +6,16 @@ class PageLinkExtractor():
     def extract_links(self, url):
         r = requests.get(url)
         start_page = BeautifulSoup(r.text, "html.parser")
-
+        links = []
         for card in start_page.select(".productWrapper .head"):
             for link in card.find_all('a', href=True):
-                yield link.get("href")
+                links.append(link.get("href"))
+        return links
 
     def extract_names(self, url):
         r = requests.get(url)
         start_page = BeautifulSoup(r.text, "html.parser")
-
+        names = []
         for card in start_page.select(".productTitleName"):
-            yield card.text
+            names.append(card.text)
+        return names
